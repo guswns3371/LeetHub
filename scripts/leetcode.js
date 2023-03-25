dde152c8540435a115de9c93ddf84e673d087abb
hjoon <guswns3371@gmail.com>
2023-03-26 00:10:25 +0900

    customize leethub

--- scripts/leetcode.js
+++ scripts/leetcode.js
@@ -67,8 +67,7 @@ const upload = (
   cb = undefined,
 ) => {
   // To validate user, load user object from GitHub.
-  const URL = `https://api.github.com/repos/${hook}/contents/${directory}/${filename}`;
-
+  const URL = `https://api.github.com/repos/${hook}/contents/LeetCode/${difficulty}/${directory}/${filename}`;
   /* Define Payload */
   let data = {
     message: msg,
@@ -137,7 +136,7 @@ const update = (
   prepend,
   cb = undefined,
 ) => {
-  const URL = `https://api.github.com/repos/${hook}/contents/${directory}/README.md`;
+  const URL = `https://api.github.com/repos/${hook}/contents/LeetCode/${difficulty}/${directory}/README.md`;
 
   /* Read from existing file on GitHub */
   const xhr = new XMLHttpRequest();
